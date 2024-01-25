dcd981a5bd51e58c6694d0179c57b32be89f4247
themms <maximusnewera@mail.ru>
2024-01-25 22:10:56 +0600

    transfering props between child and parent comp

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import TestAPI from './components/ComponentChild.vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  count: {{ count }}
+  <br/>
+  <TestAPI 
+    v-on:test="count = $event"
+    />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ComponentChild.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const value = ref(0)
+
+const returnValue = () => {
+  value.value++
+  return value.value
+}
+
+</script>
+
+<template>
+  <button 
+  v-on:click="$emit('test', returnValue())"
+  >
+  {{ value }}
+  </button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
